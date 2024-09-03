c25cec76186822968c37783655fdee95e5f9fb13
essmaw <essmay.touami@etu.u-paris.fr>
2024-09-03 15:56:19 +0200

    Add initial structure and metadata to PyClustal project.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # PyClustal
+
+<p align="center" style="width: 500px;">
+  <i> Reimplementation of Clustal Software in Python
+  </i>
+</p>
+
+## Introduction
+
+
+## Setup
+
+
+## Usage 
+
+
+## 
---
+++ src/main.py
@@ -0,0 +1,26 @@
+"""This script is the main programm of PyClustal
+
+Usage:
+======
+    python src/main.py
+"""
+
+# METADATA
+__authors__ = "Essmay Touami"
+__contact__ = "essmay.touami@etu.u-paris.fr"
+__date__ = "september 2024"
+__version__ = "1.0.0"
+
+
+# LIBRARY IMPORTS
+
+
+# CONSTANTS
+
+
+# FUNCTIONS
+
+
+# MAIN PROGRAM
+if __name__ == "__main__":
+    pass
